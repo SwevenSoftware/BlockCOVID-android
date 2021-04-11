ad73127484b8ffe061baf6b35d00c8e374b02e1d
TheAsel <theasel99@gmail.com>
2021-04-11 17:14:48 +0200

    fixed LoginViewModel tests

--- app/src/test/java/com/sweven/blockcovid/ui/login/LoginViewModelTest.kt
+++ app/src/test/java/com/sweven/blockcovid/ui/login/LoginViewModelTest.kt
@@ -2,7 +2,6 @@ package com.sweven.blockcovid.ui.login
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.*
-import com.sweven.blockcovid.Event
 import com.sweven.blockcovid.InputChecks
 import com.sweven.blockcovid.data.LoginRepository
 import org.junit.Test
@@ -11,6 +10,7 @@ import org.junit.Rule
 import org.mockito.Mockito.*
 import com.sweven.blockcovid.data.Result
 import com.sweven.blockcovid.data.model.LoggedInUser
+import org.junit.Assert.*
 
 class LoginViewModelTest {
 
@@ -30,67 +30,52 @@ class LoginViewModelTest {
 
     @Test
     fun login_success() {
-        val mockLoginResult: LiveData<LoginResult> = mock()
-        val mockServerResponse: LiveData<Event<Result<LoggedInUser>>> = mock()
         val result = Result.Success(LoggedInUser("admin", "token", 123456789, "ADMIN"))
 
         doNothing().`when`(mockLoginRepository).login("admin", "password")
-        doReturn(mockLoginResult).`when`(mockLoginViewModel).loginResult
-        doReturn(mockServerResponse).`when`(mockLoginRepository).serverResponse
 
         mockLoginRepository.triggerEvent(result)
         mockLoginViewModel.login("admin", "password")
 
-//        assertTrue(mockLoginResult.value?.success?.displayName == "admin")
-//        assertTrue(mockLoginResult.value?.success?.token == "token")
-//        assertTrue(mockLoginResult.value?.success?.expiryDate == 123456789.toLong())
-//        assertTrue(mockLoginResult.value?.success?.authority == "ADMIN")
+        assertTrue(mockLoginViewModel.loginResult.value?.success?.displayName == "admin")
+        assertTrue(mockLoginViewModel.loginResult.value?.success?.token == "token")
+        assertTrue(mockLoginViewModel.loginResult.value?.success?.expiryDate == 123456789.toLong())
+        assertTrue(mockLoginViewModel.loginResult.value?.success?.authority == "ADMIN")
     }
 
     @Test
     fun login_error() {
-        val mockLoginResult: LiveData<LoginResult> = mock()
-        val mockServerResponse: LiveData<Event<Result<LoggedInUser>>> = mock()
         val result = Result.Error("error")
 
         doNothing().`when`(mockLoginRepository).login("admin", "password")
-        doReturn(mockLoginResult).`when`(mockLoginViewModel).loginResult
-        doReturn(mockServerResponse).`when`(mockLoginRepository).serverResponse
 
         mockLoginRepository.triggerEvent(result)
         mockLoginViewModel.login("admin", "password")
 
-//        assertTrue(mockLoginResult.value?.error == "error")
+        assertTrue(mockLoginViewModel.loginResult.value?.error == "error")
     }
 
     @Test
     fun loginDataChanged_correct() {
-        val mockInputChecks: InputChecks = mock()
-        val mockLoginFormState: LiveData<LoginFormState> = mock()
-        doReturn(mockLoginFormState).`when`(mockLoginViewModel).loginFormState
-        doReturn(true).`when`(mockInputChecks).isFieldNotEmpty("username")
-        doReturn(true).`when`(mockInputChecks).isPasswordValid("password")
-
         mockLoginViewModel.loginDataChanged("username", "password")
+        assertTrue(mockLoginViewModel.loginFormState.value?.usernameError == null)
+        assertTrue(mockLoginViewModel.loginFormState.value?.passwordError == null)
+        assertTrue(mockLoginViewModel.loginFormState.value?.isDataValid!!)
     }
 
     @Test
     fun loginDataChanged_userError() {
-        val mockInputChecks: InputChecks = mock()
-        val mockLoginFormState: LiveData<LoginFormState> = mock()
-        doReturn(mockLoginFormState).`when`(mockLoginViewModel).loginFormState
-        doReturn(false).`when`(mockInputChecks).isFieldNotEmpty("")
-
         mockLoginViewModel.loginDataChanged("", "password")
+        assertTrue(mockLoginViewModel.loginFormState.value?.usernameError != null)
+        assertTrue(mockLoginViewModel.loginFormState.value?.passwordError == null)
+        assertTrue(!mockLoginViewModel.loginFormState.value?.isDataValid!!)
     }
 
     @Test
     fun loginDataChanged_passwordError() {
-        val mockInputChecks: InputChecks = mock()
-        val mockLoginFormState: LiveData<LoginFormState> = mock()
-        doReturn(mockLoginFormState).`when`(mockLoginViewModel).loginFormState
-        doReturn(false).`when`(mockInputChecks).isPasswordValid("pass")
-
         mockLoginViewModel.loginDataChanged("username", "pass")
+        assertTrue(mockLoginViewModel.loginFormState.value?.usernameError == null)
+        assertTrue(mockLoginViewModel.loginFormState.value?.passwordError != null)
+        assertTrue(!mockLoginViewModel.loginFormState.value?.isDataValid!!)
     }
 }
